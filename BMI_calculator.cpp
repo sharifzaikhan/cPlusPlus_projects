a96738bb2e6d3877dd004d09cc1bae087d7a6211
ABDUL MAJED KHAN <103608952+sharifzaikhan@users.noreply.github.com>
2023-09-05 04:14:48 -0400

    Update BMI_calculator.cpp

--- BMI_calculator.cpp
+++ BMI_calculator.cpp
@@ -1,1 +1,11 @@
-
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+  float w,h;
+cout<<"Enter your weight in Kg: "<<endl;
+cin>>w;
+cout<<"Enter your Height in meter : "<<endl;
+cin>>h;
+float BMI = w/(pow(h,2));
+cout<<"Your BMI is " << BMI <<endl;
